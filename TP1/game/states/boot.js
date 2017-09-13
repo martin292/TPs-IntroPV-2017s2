013619906b8ab58a524429063200a8d3104dcaee
martin <martin.ramos292@gmail.com>
2017-09-13 20:14:55 -0300

    Agrego txt al estado boot

--- TP1/game/states/boot.js
+++ TP1/game/states/boot.js
@@ -10,6 +10,10 @@ var boot = {
 		button = game.add.button(400, 300, 'button', onClick);
 		button.anchor.setTo(0.5);
 		button.scale.setTo(0.5);
+
+		var style = { font: "32px Courier", fill: "#ffffff" };
+
+		var text1 = game.add.text(2, 1, "IntroPV - Minion 1", style);	
 	},
 
 	update: function() {}
